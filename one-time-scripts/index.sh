2b434a41c6a6bce29086f615ae66baa39dd9da06
Vadym <vadym@Vadyms-MacBook-Air.local>
2023-03-31 22:55:13 -0500

    Dev/Create mongodb secret

--- one-time-scripts/index.sh
+++ one-time-scripts/index.sh
@@ -1,6 +1,10 @@
 aws eks update-kubeconfig --region us-west-2 --name search
+
+kubectl create namespace scrapper
+kubectl create secret generic zenrows-key --from-literal=ZENROWS_KEY=042572b0dad995a60dd8be884ec746290fa7d10c --namespace=scrapper
+kubectl create secret generic mongodb-uri --from-literal=MONGODB_URI=mongodb://scrapper:123456@mongodb.mongodb.svc.cluster.local:27017/scrapper --namespace=scrapper
+
 helm install helm helm
 kubectl create secret docker-registry ghcr-secret --docker-server=ghcr.io --docker-username=danxil --docker-password=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 --namespace=scrapper
 kubectl create secret docker-registry ghcr-secret --docker-server=ghcr.io --docker-username=danxil --docker-password=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 --namespace=search
 kubectl create secret docker-registry ghcr-secret --docker-server=ghcr.io --docker-username=danxil --docker-password=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 --namespace=ui
-kubectl create secret generic zenrows-key --from-literal=ZENROWS_KEY=042572b0dad995a60dd8be884ec746290fa7d10c --namespace=scrapper
